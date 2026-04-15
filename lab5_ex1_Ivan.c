a1e65f3b1c48e1d46d77df6a94f6059914dd2141
vaipanivan72 <vaipanion646@gmail.com>
2026-04-15 19:23:27 +0300

    Update lab5_ex1_Ivan.c

--- lab5_ex1_Ivan.c
+++ lab5_ex1_Ivan.c
@@ -1,23 +1,55 @@
 #include <stdio.h>
 
 int main() {
-    int n, count = 0;
-    printf("Введіть кількість елементів: ");
-    scanf("%d", &n);
+    int choice;
+    int n, i, count;
 
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        printf("a[%d] = ", i);
-        scanf("%d", &a[i]);
-    }
+    do {
+        // Вивід меню
+        printf("\n========== МЕНЮ ==========\n");
+        printf("1. Ввести масив\n");
+        printf("0. Вихід\n");
+        printf("==========================\n");
+        printf("Ваш вибір: ");
+        scanf("%d", &choice);
 
-    // Перевірка сусідніх елементів на різні знаки
-    for (int i = 0; i < n - 1; i++) {
-        if ((a[i] > 0 && a[i+1] < 0) || (a[i] < 0 && a[i+1] > 0)) {
-            count++;
+        switch (choice) {
+            case 1: {
+                // Основна логіка вашого коду
+                printf("Введіть кількість елементів: ");
+                scanf("%d", &n);
+
+                int a[n];
+                count = 0;
+
+                // Введення елементів
+                for (i = 0; i < n; i++) {
+                    printf("a[%d] = ", i);
+                    scanf("%d", &a[i]);
+                }
+
+                // Перевірка сусідніх елементів на різні знаки
+                for (i = 0; i < n - 1; i++) {
+                    if ((a[i] > 0 && a[i+1] < 0) || (a[i] < 0 && a[i+1] > 0)) {
+                        count++;
+                    }
+                }
+
+                printf("\nРезультат: Кількість сусідств з різними знаками: %d\n", count);
+                break;
+            }
+
+            case 0:
+                printf("Завершення роботи...\n");
+                break;
+
+            default:
+                printf("Помилка: такого пункту не існує. Спробуйте ще раз.\n");
+                break;
         }
-    }
 
-    printf("Кількість сусідств з різними знаками: %d\n", count);
+    } while (choice != 0);
+
     return 0;
 }
+
